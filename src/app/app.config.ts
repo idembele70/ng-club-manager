30ac76f76a85ff10b2aa331c5f8f219d002d73bf
idembele70 <idembele70@gmail>
2025-12-24 21:20:13 +0100

    feat(i18n): setup translation for english & french
    Refs: #2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { i18nProviders } from './core/config/i18n.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    ...i18nProviders,
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { SUPPORTED_LANGS, DEFAULT_LANG } from './core/config/i18n.config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    TranslatePipe,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ng-club-manager');
+export class App implements OnInit {
+    private readonly translateService = inject(TranslateService);
+
+  ngOnInit(): void {
+    this.loadLang();
+  }
+
+  private loadLang() {
+    this.translateService.addLangs([...SUPPORTED_LANGS]);
+    const browserLang = this.translateService.getBrowserLang();
+    const langRegex = new RegExp(SUPPORTED_LANGS.join('|'));
+    const lang = browserLang?.match(langRegex) ? browserLang : DEFAULT_LANG;
+    this.translateService.use(lang);
+  }
 }
